df85c5548f4234d4f0c5efcd0cf48901bd9d8dbe
Joshua Atanu <atanu.joshua@gmail.com>
2023-02-19 20:22:14 +0100

    fix api key error and update the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
-import { Configuration, OpenAIApi}from "openai"
+import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
-  console.log(import.meta.env.OPENAI_API_KEY);
-  // const configuration = new Configuration({
-  //   apiKey: process.env.OPENAI_API_KEY,
-  // });
 
+  console.log(import.meta.env.VITE_OPENAI_API_KEY);
+  
+  const configuration = new Configuration({
+    apiKey:import.meta.env.VITE_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  const generateImage = async () => {
+    const res = await openai.createCompletion({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024",
+    });
+    console.log(res.data.data[0].url);
+  };
 
-  return <div></div>;
+  return <div>
+    <button onClick={generateImage}>generate an image</button>
+  </div>;
 }
 
 export default App;
