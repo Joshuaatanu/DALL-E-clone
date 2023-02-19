9c0b39e68dfa33ce171832db4e087b20f6bc1925
Joshua Atanu <atanu.joshua@gmail.com>
2023-02-19 20:49:00 +0100

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,35 @@ import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState('')
+  // console.log(import.meta.env.VITE_OPENAI_API_KEY);
 
-  console.log(import.meta.env.VITE_OPENAI_API_KEY);
-  
   const configuration = new Configuration({
-    apiKey:import.meta.env.VITE_OPENAI_API_KEY,
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
   });
   const openai = new OpenAIApi(configuration);
   const generateImage = async () => {
-    const res = await openai.createCompletion({
-      prompt: "a white siamese cat",
+    const res = await openai.createImage({
+      prompt: prompt,
       n: 1,
       size: "1024x1024",
     });
-    console.log(res.data.data[0].url);
+    setResult(res.data.data[0].url);
   };
 
-  return <div>
-    <button onClick={generateImage}>generate an image</button>
-  </div>;
+  return (
+    <div className="app-main">
+      <h3>Generate an image using OpenAI Api</h3>
+      <input
+      placeholder="Type something to generate an image"
+        className="app-input"
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button onClick={generateImage}>generate an image</button>
+      {result.length > 0 ?<img src={result} alt="result" className="result-image"/>:<></>}
+    </div>
+  );
 }
 
 export default App;
