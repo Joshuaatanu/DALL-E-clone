53985983ba2b70db9d4ec2733fe13e9d89789ca9
Joshua Atanu <atanu.joshua@gmail.com>
2023-02-19 19:46:40 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Configuration, OpenAIApi}from "openai"
+import "./App.css";
+
+function App() {
+  console.log(import.meta.env.OPENAI_API_KEY);
+  // const configuration = new Configuration({
+  //   apiKey: process.env.OPENAI_API_KEY,
+  // });
+
+
+  return <div></div>;
+}
+
+export default App;
